7ec94275d8e934ef87093b78c3801644713ed07c
zcvfcat <zcvfcat@gmail.com>
2023-04-23 23:24:19 +0900

    feat (publish)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: process.cwd(),
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin', 'react'],
